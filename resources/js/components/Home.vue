d9138ccb11d105a3e7796872afa912e22f8cae0a
Temidayo <temidayojunior@gmail.com>
2018-09-25 16:35:50 +0100

    Adding vue componens and some frontend work

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,38 @@
+<template>
+            <nav class="panel column is-offset-2 is-8">
+        <p class="panel-heading">
+            Population records
+
+            <button class="button is-link is-outlined">
+                 Add new city
+            </button>
+        </p>
+        <div class="panel-block">
+            <p class="control has-icons-left">
+            <input class="input is-small" type="text" placeholder="search">
+            <span class="icon is-small is-left">
+                <i class="fas fa-search" aria-hidden="true"></i>
+            </span>
+            </p>
+        </div>
+        
+        <a class="panel-block">
+           
+           <span class="panel-icon column is-9">
+               marksheet
+            </span>
+
+            <span class="panel-icon column is-1">
+                <i class="has-text-danger fas fa-trash" aria-hidden="true"></i>
+            </span>
+
+            <span class="panel-icon column is-1">
+                <i class="has-text-info fas fa-edit" aria-hidden="true"></i>
+            </span>
+
+            <span class="panel-icon column is-1">
+                <i class="has-text-priamry fas fa-eye" aria-hidden="true"></i>
+            </span>
+        </a>
+        </nav>
+</template>
---
+++ resources/js/components/Myfooter.vue
@@ -0,0 +1,11 @@
+<template>
+    <footer class="footer">
+        <div class="content has-text-centered">
+            <p>
+            <strong>Phonebook</strong> by <a href="https://jgthms.com">Temidayo</a>. The source code is on 
+            <a href="http://opensource.org/licenses/mit-license.php">GITHUB</a>. The website content
+            is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+            </p>
+        </div>
+    </footer>
+</template>
